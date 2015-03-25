2c2be09752538ee146fde795a5109be6a0260605
bholzer <brennan.holzer@complymd.com>
2015-03-25 16:22:31 -0500

    initial locomotive setup

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,24 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret '9fca9cb7b8094e853a21c3eccaed514cf4ce66e477951a592ea69c6e229085ab'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NormalJacketSite::Application.config.secret_token = 'f3fd94e929cbb14d86a0a6a924bb6ec46b8a358a63fd57d13414738ea6efc092f76c473f1c6a0d64028b4339b94ef5d836831e6174fbd4cf12a46a50dd6b4c01'
