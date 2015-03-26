95d0bf248dba9841630c23aae8e3d1c8c241b68c
bholzer <brennan.holzer@complymd.com>
2015-03-25 22:12:09 -0500

    Clear it

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-NormalJacketSite::Application.config.session_store :cookie_store, key: '_normal_jacket_site_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# NormalJacketSite::Application.config.session_store :active_record_store
