4416ba76cbb54260cfdb463cc02249a56b5f8553
bholzer <brennan.holzer@complymd.com>
2015-04-07 19:47:53 -0500

    change scripts

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){	
+$(document).ready(function(){
 
 /***************************************************
 	MENU
@@ -59,7 +59,9 @@ $("[data-rel=tooltip]").tooltip();
 				event.preventDefault();
 				
 				$('html, body').animate({scrollTop: 0}, 300);
-			})
+			});
+
+	
 		});	
 
 /***************************************************
